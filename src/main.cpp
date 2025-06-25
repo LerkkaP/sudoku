6463d149143e45f13055713f46a0d264734815ff
Erik Peteri <peteri.erik@gmail.com>
2025-06-25 15:36:21 +0300

    Create hello world program using sfml

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello world\n";
+    sf::RenderWindow window(sf::VideoMode(400, 300), "Hello world!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::Blue);
+        window.display();
+    }
     return 0;
 }
